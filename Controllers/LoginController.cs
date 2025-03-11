92dfc2a17f1ce726ea0878b813e33b6239f1eb0a
schele <carl_schele@hotmail.com>
2025-03-11 10:58:06 +0100

    creating a login page, moved html markup

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using gms2025.Models.PublishedModels;
+using GMS2025.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace GMS2025.Controllers
+{
+    public class LoginController : RenderController
+    {
+        public LoginController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine, umbracoContextAccessor)
+        {
+        }
+
+        public override IActionResult Index()
+        {
+            var loginPage = CurrentPage as Login;
+
+            if (loginPage != null)
+            {
+                var model = new LoginPageViewModel(loginPage);
+
+                return CurrentTemplate(model);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ Models/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using gms2025.Models.PublishedModels;
+using Umbraco.Cms.Core.Web;
+
+namespace GMS2025.Models.ViewModels
+{
+    public class LoginPageViewModel : BasePageViewModel<Login>
+    {
+        public LoginPageViewModel(Login content) : base(content, StaticServiceProvider.Instance.GetService<IUmbracoContextAccessor>())
+        {
+        }
+    }
+}
